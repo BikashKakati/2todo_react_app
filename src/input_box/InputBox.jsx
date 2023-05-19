95056e1f55f1e4e850b750c0d3a00031150551cc
BikashKakati <bikashkakati796@gmail.com>
2023-05-19 23:57:57 +0530

    Add task by pressing Enter
    Now user can add task using Enter key on keyboard
    - using onKeyUp event handler we add this adding feature

--- src/input_box/InputBox.jsx
+++ src/input_box/InputBox.jsx
@@ -5,6 +5,12 @@ import { Context } from "../App"
 const InputBox = () => {
   const {inputData, setInputData,taskList, setTaskList, editItemId, setEditItemId} = useContext(Context)
 
+  const handleAdd = (e) =>{
+    if(e.key === "Enter"){
+      addTask()
+    }
+  }
+
   const addTask = () =>{
     if(!inputData){
       alert("Input Field is empty")
@@ -22,13 +28,14 @@ const InputBox = () => {
       setInputData("")
     }
   }
+
   return (
     <div className="input-container">
 
-      <input type="text" placeholder="write your new task" value={inputData} onChange={(data) => setInputData(data.target.value)}></input>
+      <input type="text" placeholder="write your new task" value={inputData} onKeyUp = {handleAdd} onChange={(data) => setInputData(data.target.value)}></input>
+      
       {editItemId ? (<button onClick={addTask}><i className="fas fa-edit"></i></button>) : (<button onClick={addTask}><i className="fas fa-plus"></i></button>)}
-
-
+      
     </div>
   )
 }
